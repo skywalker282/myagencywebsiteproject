2e547ad8692ecdd7f58d18235da3e1092a6d8134
Crispin Cirhuza Zagabe <celiitellu@gmail.com>
2021-09-06 11:31:59 +0200

    Feat: Update service worker cache strategy

--- main-worker.js
+++ main-worker.js
@@ -16,9 +16,9 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     (async () => {
-      // if ("navigationPreload" in self.registration) {
-      //   await self.registration.navigationPreload.enable();
-      // }
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
     })()
   );
   self.clients.claim();
@@ -32,23 +32,29 @@ self.addEventListener("fetch", (event) => {
       (async () => {
         try {
           // First, try to use the navigation preload response if it's supported.
-          // const preloadResponse = await event.preloadResponse;
-          // if (preloadResponse) {
-          //   return preloadResponse;
-          // }
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
 
           const networkResponse = await fetch(event.request);
           return networkResponse;
+        
         } catch (error) {
           // catch is only triggered if an exception is thrown, which is likely
           // due to a network error.
           // If fetch() returns a valid HTTP response with a response code in
           // the 4xx or 5xx range, the catch() will NOT be called.
-          console.log("Fetch failed; returning offline page instead.", error);
-
+         
           const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+          const cachedResponse = await cache.match(event.request);
+          if (cachedResponse) {
+            return cacheResponse
+          }
+
+          const offlineResponse = await cache.on(OFFLINE_URL);
+          
+          return offlineResponse;
         }
       })()
     );
